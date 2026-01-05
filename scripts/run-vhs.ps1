399edd835c8649e5bde914ac0737e5e73ffbfa27
Raj <rajvishwakarma303@gmail.com>
2026-01-05 19:36:28 +0530

    chore: remove unused demo scripts

--- scripts/run-vhs.ps1
+++
@@ -1,2 +0,0 @@
-$env:PATH = $env:PATH + ";C:\Users\rajvi\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.0.1-full_build\bin"
-& "C:\Users\rajvi\AppData\Local\Microsoft\WinGet\Packages\charmbracelet.vhs_Microsoft.Winget.Source_8wekyb3d8bbwe\vhs_0.10.0_Windows_x86_64\vhs.exe" "scripts\demo.tape"
